c78da77d48e7bd717639b1f2237096810b60b331
howels <howels@users.noreply.github.com>
2014-07-27 21:54:39 +0000

    Moved new bluetooth app and tweaked mjpg streamer into start.sh, replaced app.js with new content

--- app.js
+++ app.js
@@ -2,107 +2,247 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    routes = require('./routes'),
-    sio = require('socket.io'),
-    gpio = require('pi-gpio'),
-    crypto = require('crypto'),
-    async = require('async'),
+var express = require('express')
+  , http = require('http');
+var app = express(); //- ONLY FOR NEWER 3.x express
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var dualShock = require('dualshock-controller');
+ 
+var async = require('async'),
+    five = require("johnny-five"),
+    raspi = require('raspi-io'),
     tank = {},
-    _leftMotorFront  = 11,
-    _leftMotorBack   = 12,
-    _rightMotorFront = 15,
-    _rightMotorBack  = 16,
-    app = module.exports = express.createServer(),
-    io = sio.listen(app);
+    _leftMotorFront  = 7,
+    _leftMotorBack   = 11,
+    _rightMotorFront = 12,
+    _rightMotorBack  = 13,
+    _turnTime = 100,
+    _speed = 1; //only possible when using arduino when we can use analogWrite instead of digitalWrite
 
-// Configuration
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+//this is to use the Pi's GPIO pins:
+var board = new raspi();
+//var board = new five.Board({
+//	io: new raspi()
+//});
+
+board.on("ready", function() {
+
+this.pinMode(7, board.MODES.OUTPUT);
+this.pinMode(11, board.MODES.OUTPUT);
+this.pinMode(12, board.MODES.OUTPUT);
+this.pinMode(13, board.MODES.OUTPUT);
+
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, 0) },
+   function(callback) { board.digitalWrite(_leftMotorBack, 0) },
+   function(callback) { board.digitalWrite(_rightMotorFront, 0) },
+   function(callback) { board.digitalWrite(_rightMotorBack, 0) }],
+   function(err, results) {
+     res.end();
+   }
+ );
+
 });
 
-app.configure('development', function() {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+// Access server through port 80
+server.listen(3000);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
+
+// Set '/public' as the static folder. Any files there will be directly sent to the viewer
+app.use(express.static(__dirname + '/public'));
+
+// Set index.html as the base file
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index4.html');
 });
 
-app.configure('production', function() {
-  app.use(express.errorHandler());
-});
-
-// Routes
-app.get('/', routes.index);
-
-app.listen(3000);
-console.log('Listening %d in %s mode', app.address().port, app.settings.env);
 
 tank.initPins = function(){
-  async.parallel([
-    gpio.open(_leftMotorFront),
-    gpio.open(_leftMotorBack),
-    gpio.open(_rightMotorFront),
-    gpio.open(_rightMotorBack)
-  ]);
+
+console.log("Tank ready");
+
 };
 
 tank.moveForward = function(){
-  async.parallel([
-    gpio.write(_leftMotorFront, 1),
-    gpio.write(_rightMotorFront, 1)
-  ]);
+ console.log("Forward function");
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, _speed) },                                    
+   function(callback) { board.digitalWrite(_leftMotorBack, 0) },
+   function(callback) { board.digitalWrite(_rightMotorFront, _speed) },
+   function(callback) { board.digitalWrite(_rightMotorBack, 0) }],
+   function(err, results) {
+     res.end();
+   }
+ );
 };
 
 tank.moveBackward = function(){
-  async.parallel([
-    gpio.write(_leftMotorBack, 1),
-    gpio.write(_rightMotorBack, 1)
-  ]);
+ console.log("Backward function");
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, 0) },                                    
+   function(callback) { board.digitalWrite(_leftMotorBack, _speed) },
+   function(callback) { board.digitalWrite(_rightMotorFront, 0) },
+   function(callback) { board.digitalWrite(_rightMotorBack, _speed) }],
+   function(err, results) {
+     res.end();
+   }
+ );
 };
 
 tank.turnLeft = function(){
-  gpio.write(_rightMotorFront, 1);
+ console.log("Turn left function");
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, 0) },                                    
+   function(callback) { board.digitalWrite(_leftMotorBack, _speed) },
+   function(callback) { board.digitalWrite(_rightMotorFront, _speed) },
+   function(callback) { board.digitalWrite(_rightMotorBack, 0) }],
+   function(err, results) {
+     res.end();
+   }
+ );
+};
+
+tank.turnLeftShort = function(){
+ tank.turnLeft();
+ setTimeout(tank.stopAllMotors, _turnTime);
 };
 
 tank.turnRight = function(){
-  gpio.write(_leftMotorFront, 1);
+ console.log("Turn right function");
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, _speed) },                                    
+   function(callback) { board.digitalWrite(_leftMotorBack, 0) },
+   function(callback) { board.digitalWrite(_rightMotorFront, 0) },
+   function(callback) { board.digitalWrite(_rightMotorBack, _speed) }],
+   function(err, results) {
+     res.end();
+   }
+ );
+};
+
+tank.turnRightShort = function(){
+ tank.turnRight();
+ setTimeout(tank.stopAllMotors, _turnTime);
 };
 
 tank.stopAllMotors = function(){
-  async.parallel([
-    gpio.write(_leftMotorFront, 0),
-    gpio.write(_leftMotorBack, 0),
-    gpio.write(_rightMotorFront, 0),
-    gpio.write(_rightMotorBack, 0)
-  ]);
+ console.log("Stop function");
+ async.parallel([function(callback) { board.digitalWrite(_leftMotorFront, 0) },                                    
+   function(callback) { board.digitalWrite(_leftMotorBack, 0) },
+   function(callback) { board.digitalWrite(_rightMotorFront, 0) },
+   function(callback) { board.digitalWrite(_rightMotorBack, 0) }],
+   function(err, results) {
+     res.end();
+   }
+ );
 };
+
 
 io.sockets.on('connection', function(socket) {
   
-  socket.on('keydown', function(dir) {
-    switch(dir){
-     case 'up':
-        tank.moveForward();
-        break;
-      case 'down':
-        tank.moveBackward();
-        break;
-      case 'left':
-        tank.turnLeft();
-        break;
-      case 'right':
-        tank.turnRight();
-        break;
+  console.log("Socket.IO connected");
+  socket.emit('robot connected', { data: 'Connected' });
+
+  socket.on('robot command', function (data) {
+    //processRobotCommand (data.data);
+    console.log("Received command: " + data.command);
+    var command = data.command;
+    if (command == 'forward') {
+      tank.moveForward();
+    }
+    if (command == 'reverse') {
+      tank.moveBackward();
+    }
+    if (command == 'turn-left') {
+      tank.turnLeftShort();
+    }
+    if (command == 'turn-right') {
+      tank.turnRightShort();
+    }
+    if (command == 'stop') {
+      tank.stopAllMotors();
     }
   });
 
-  socket.on('keyup', function(dir){
+
+});
+
+  //pass options to init the controller.
+  var controller = dualShock(
+      {
+          //you can use a ds4 by uncommenting this line.
+          //config: "dualshock4-generic-driver",
+          //if using ds4 comment this line.
+          config : "dualShock3",
+          //smooths the output from the acelerometers (moving averages) defaults to true
+          accelerometerSmoothing : true,
+          //smooths the output from the analog sticks (moving averages) defaults to false
+          analogStickSmoothing : false
+      });
+
+  //make sure you add an error event handler
+  controller.on('error', function(data) {
+    console.log("Controller has done a whoopsie")
+    //...someStuffDidNotWork();
+  });
+
+  //add event handlers:
+  controller.on('left:move', function(data) {
+    //...doStuff();
+  });
+  controller.on('right:move', function(data) {
+    //...doStuff();
+  });
+
+  controller.on('connected', function(data) {
+    console.log("dualShock3 connected")
+  });
+  controller.on('dpadUp:press', function (data) {
+    tank.moveForward();
+  });
+  controller.on('dpadUp:release', function (data) {
     tank.stopAllMotors();
   });
 
-});
+  controller.on('dpadLeft:press', function (data) {
+    tank.turnLeft();
+  });
+  controller.on('dpadLeft:release', function (data) {
+    tank.stopAllMotors();
+  });
+
+  controller.on('dpadDown:press', function (data) {
+    tank.moveBackward();
+  });
+  controller.on('dpadDown:release', function (data) {
+    tank.stopAllMotors();
+  });
+
+  controller.on('dpadRight:press', function (data) {
+    tank.turnRight();
+  });
+  controller.on('dpadRight:release', function (data) {
+    tank.stopAllMotors();
+  });
+
+
+  //controller status
+  //as of version 0.6.2 you can get the battery %, if the controller is connected and if the controller is charging
+  controller.on('battery:change', function (value) {
+       //...doStuff();
+  });
+  controller.on('connection:change', function (value) {
+       console.log("Connection state changed")
+  });
+  controller.on('charging:change', function (value) {
+       //...doStuff();
+  });
+
+  //connect the controller
+  controller.connect();
+
+
+
 
 tank.initPins();
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 node app.js &
 
 # start streaming server
-mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0  -r 640x480 -f 30" -o "/usr/lib/output_http.so -p 8090 -w /var/www/mjpg_streamer" && fg
+mjpg_streamer -o "/usr/local/lib/output_http.so -w ./www" -i "/usr/local/lib/input_raspicam.so -ISO 800 -x 640 -y 480 -fps 15 -quality 10 -vf -hf -ex night"
